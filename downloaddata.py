46403402757b542cfb9e88f65eb90725f31b2cff
bmvshetty <141489259+bmvshetty@users.noreply.github.com>
2025-04-12 13:28:10 +0530

    Add files via upload

---
+++ downloaddata.py
@@ -0,0 +1,12 @@
+import urllib
+import urllib.request
+import os
+
+
+
+
+file_path = os.path.join("datasets","housing")
+os.makedirs(file_path,exist_ok=True)
+file_path = os.path.join(file_path,"housing.csv")
+
+urllib.request.urlretrieve("https://raw.githubusercontent.com/bjnaga/ML-lab/refs/heads/main/datasets/housing.csv", file_path)
